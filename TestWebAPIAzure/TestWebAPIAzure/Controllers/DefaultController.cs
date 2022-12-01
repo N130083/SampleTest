59d0beb5224b87f29f2b27269cab6cc3d1105aba
Noorjahan Shaik <noorjahan.iiit@gmail.com>
2022-12-01 15:27:14 +0000

    sample

---
+++ TestWebAPIAzure/TestWebAPIAzure/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace TestWebAPIAzure.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,"Hello this is printed");
+        }
+    }
+}
